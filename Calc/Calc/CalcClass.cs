3df7a0b563002ef2898bc34fff9f7538a0a5b8b4
지 창호 <shio0145@gmail.com>
2018-10-22 10:33:18 +0900

    .

--- Calc/Calc/CalcClass.cs
+++ Calc/Calc/CalcClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace Calc
 {
     // 계산 클래스
@@ -12,32 +13,41 @@ namespace Calc
         {
             ListExpr = listExpr;
         }
+
         // 입력된 전체수식 리스트
         private List<string> ListExpr { get; set; }
+
         // 입력된 전체수식 계산
         public float Calculation()
         {
             int index = 0;
+
             // 곱셈
             while (true)
             {
                 index = ListExpr.FindIndex(x => x == "*");
+
                 if (index == -1) break;
+
                 // 곱셈 후 불필요 List 아이템 삭제
                 ListExpr[index - 1] = (float.Parse(ListExpr[index - 1]) * float.Parse(ListExpr[index + 1])).ToString();
                 ListExpr.RemoveAt(index);
                 ListExpr.RemoveAt(index);
             }
+
             // 나눗셈
             while (true)
             {
                 index = ListExpr.FindIndex(x => x == "/");
+
                 if (index == -1) break;
+
                 // 나눗셈 후 불필요 List 아이템 삭제
                 ListExpr[index - 1] = (float.Parse(ListExpr[index - 1]) / float.Parse(ListExpr[index + 1])).ToString();
                 ListExpr.RemoveAt(index);
                 ListExpr.RemoveAt(index);
             }
+
             // 덧셈 + 뺄셈 ------ TODO 덧셈 따로, 뺄셈 따로 프로세스 문제에 대해 설명 필요
             while (true)
             {
@@ -48,10 +58,12 @@ namespace Calc
                     {
                         ListExpr[0] = (float.Parse(ListExpr[0]) + float.Parse(ListExpr[2])).ToString();
                     }
+
                     if (ListExpr[1] == "-")
                     {
                         ListExpr[0] = (float.Parse(ListExpr[0]) - float.Parse(ListExpr[2])).ToString();
                     }
+
                     ListExpr.RemoveAt(1);
                     ListExpr.RemoveAt(1);
                 }
@@ -63,6 +75,7 @@ namespace Calc
             return float.Parse(ListExpr[0]);
         }
     }
+
     // 입력값 검증 및 입력 클래스
     class Validate
     {
@@ -70,22 +83,33 @@ namespace Calc
         {
             ListExpr = listExpr;
         }
+
         // 입력된 전체수식 리스트
         private List<string> ListExpr { get; set; }
+
         public void Validatiton()
         {
-            // TODO 입력 숫자 맥스값 지정 필요
-            // TODO 소숫점 입력 가능하도록 수정
             ConsoleKeyInfo cki;
+
             string iStr = "";
+
             while (true)
             {
                 cki = Console.ReadKey(true);
-                if (cki.KeyChar >= '0' && cki.KeyChar <= '9') // 숫자
+
+                if ((cki.KeyChar >= '0' && cki.KeyChar <= '9') || cki.KeyChar == '.') // 숫자
                 {
                     if (cki.KeyChar == '0')
                     {
                         if (ListExpr.Count == 0 || ListExpr[ListExpr.Count - 1] != "/")   // 0으로 나누기 안됨
+                        {
+                            Console.Write(cki.KeyChar);
+                            iStr += cki.KeyChar;
+                        }
+                    }
+                    else if (cki.KeyChar == '.')
+                    {
+                        if (iStr != "")
                         {
                             Console.Write(cki.KeyChar);
                             iStr += cki.KeyChar;
@@ -102,8 +126,13 @@ namespace Calc
                 }
                 else if (cki.KeyChar == '*' || cki.KeyChar == '/' || cki.KeyChar == '+' || cki.KeyChar == '-') // 연산자
                 {
-                    if (iStr != "") // 연산자 중복 입력 방지
+                    // 연산자 앞에 . 은 올수 없음 ex) 1+20.+
+                    // 연산자 중복 입력 방지
+                    if ((iStr != "") && (iStr.Substring(iStr.Length - 1) != "."))
                     {
+                        // 숫자 검증
+                        CheckNumber(iStr);
+
                         ListExpr.Add(iStr);
                         ListExpr.Add(cki.KeyChar.ToString());
                         iStr = "";
@@ -119,22 +148,43 @@ namespace Calc
                 }
                 else if (iStr != "" && cki.KeyChar == '=') // 수식 계산
                 {
+                    // 숫자 검증
+                    CheckNumber(iStr);
+
                     ListExpr.Add(iStr);
                     ListExpr.Add(cki.KeyChar.ToString());
                     break;
                 }
             }
         }
+
+        private void CheckNumber(string value)
+        {
+            // 숫자 검증
+            try
+            {
+                float.Parse(value);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("");
+                Console.WriteLine(e.Message);
+                Environment.Exit(0);
+            }
+        }
     }
+
     // 계산결과 내역 클래스
     static public class History
     {
         static private List<string> ListHistory = new List<string>();
+
         // 계산결과 저장하기
         static public void AddHistory(string history)
         {
             ListHistory.Add(history);
         }
+
         // 계산결과 내역 보기
         static public void ShowHistory()
         {
@@ -144,6 +194,7 @@ namespace Calc
             }
         }
     }
+
     // 숫자 꾸미기
     static public class Deco
     {
@@ -151,6 +202,7 @@ namespace Calc
         static public string AddComma(string number)
         {
             string rtnValue = "";
+
             if (number == "*" || number == "/" || number == "+" || number == "-" || number == "=")
             {
                 rtnValue = number;
@@ -159,8 +211,10 @@ namespace Calc
             {
                 rtnValue = string.Format("{0:#,##0.##}", float.Parse(number));
             }
+
             return rtnValue;
         }
+
         // 숫자에 콤마 빼기
         static public string RemoveComma(string number)
         {
--- Calc/Calc/Program.cs
+++ Calc/Calc/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace Calc
 {
     class Program
@@ -11,7 +12,8 @@ namespace Calc
         {
             MainMenu();
         }
-        // 메인메뉴
+
+        // 메인메뉴.
         static void MainMenu()
         {
             Console.Clear();
@@ -19,7 +21,9 @@ namespace Calc
             Console.WriteLine("1. 이전계산식 전체조회 ");
             Console.WriteLine("2. 계산기");
             Console.WriteLine("3. 종료");
+
             string inNum = Console.ReadLine();
+
             switch (inNum)
             {
                 case "1":
@@ -35,18 +39,23 @@ namespace Calc
                     break;
             }
         }
+
         // 계산 내역 조회
         static void ShowHistory()
         {
             Console.Clear();
             Console.WriteLine("계산 내역");
+
             History.ShowHistory();
+
             Console.WriteLine("\n");
             Console.WriteLine("메뉴를 선택하세요");
             Console.WriteLine("1. 메인메뉴 ");
             Console.WriteLine("2. 계산기");
             Console.WriteLine("3. 종료");
+
             string inNum = Console.ReadLine();
+
             switch (inNum)
             {
                 case "1":
@@ -62,6 +71,7 @@ namespace Calc
                     break;
             }
         }
+
         // 계산기
         static void Calculator()
         {
@@ -70,33 +80,46 @@ namespace Calc
             // 숫자와 +,-,*,-,= 을 제외한 문자 입력 방지
             // 숫자 다음에 숫자, 연산자 다음에 연산자는 올수 없음
             List<string> listExpr = new List<string>();
+
             Console.Clear();
             Console.Write("계산식: ");
             Validate val = new Validate(listExpr);
+
             val.Validatiton();
+
             // 계산 결과
             Console.Clear();
             Console.Write("계산결과: ");
+
             string sExpr = "";
+
             for (int i = 0; i < listExpr.Count; i++)
             {
                 sExpr += Deco.AddComma(listExpr[i]);
             }
+
             Calculate calc = new Calculate(listExpr);
+
             // 수식 계산 값
             sExpr += Deco.AddComma(calc.Calculation().ToString());
+
             Console.WriteLine(sExpr);
+
             // 계산 날짜 추가
             sExpr = "(" + DateTime.Now + ") " + sExpr;
+
             // 계산 내역 저장
             History.AddHistory(sExpr);
+
             Console.WriteLine();
             Console.WriteLine("메뉴를 선택하세요");
             Console.WriteLine("1. 메인메뉴");
             Console.WriteLine("2. 이전계산식 전체조회 ");
             Console.WriteLine("3. 계산기");
             Console.WriteLine("4. 종료");
+
             string inNum = Console.ReadLine();
+
             switch (inNum)
             {
                 case "1":
